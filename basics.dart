05d157e9d4473380f72be51d0b063375e331072a
Anurag Kumar Jha <162889558+Anuragzha@users.noreply.github.com>
2025-09-09 00:58:06 +0545

    Create basics.dart

---
+++ basics.dart
@@ -0,0 +1,20 @@
+// swappping values
+// void main(){
+//   int x = 10;
+//   int y = 20;
+//   print("Before Swapping: x =$x, y = $y");
+//   int temp = x;
+//   x = y;
+//   y = temp;
+//   print("After swapping: x=$x, y=$y");
+// }
+
+// // feet into meter and meter into km
+// void main(){
+//   double feet =5000;
+//   double meter, km;
+//   meter = feet /3;
+//   km = meter/1000;
+//   print("meters: $meter");
+//   print("Kilometers: $km");
+// }
