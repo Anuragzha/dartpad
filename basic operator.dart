df139f67c6bec019541ac3eb33f369f20975e10b
Anurag Kumar Jha <162889558+Anuragzha@users.noreply.github.com>
2025-09-09 14:40:29 +0545

    Create basic operator.dart

---
+++ basic operator.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 50;
+  int y = 10;
+  int sum = x + y;
+  int difference = x - y;
+  int product = x * y;
+  double quotient = x / y;
+
+  print("The sum of $x and $y is $sum");
+  print("The difference when $y is subtracted from $x is $difference");
+  print("The product of $x and $y is $product");
+  print("The quotient when $x is divided by $y is $quotient");
+}
